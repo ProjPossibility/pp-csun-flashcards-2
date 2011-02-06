334d7dd53b14c7f317abfb0cc04266604c905998
nourvan@gmail.com <nourvan@gmail.com@70de9bcd-8a5d-4bb2-0ea2-7be2089eee11>
2011-02-06 03:03:19 +0000

    Added baseclass db handler started work on deckslist

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import tornado.auth
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
+import tornado.database
 import tornado.web
 import os.path
 
@@ -9,12 +10,20 @@ import os.path
 from tornado.options import define, options
 
 define("port", default=8888, help="run on the given port", type=int)
+define("mysql_host", default="127.0.0.1:3306", help="blog database host")
+define("mysql_database", default="blog", help="blog database name")
+define("mysql_user", default="root", help="blog database user")
+define("mysql_password", default="fatass", help="blog database password")
 
 class HomeHandler(tornado.web.RequestHandler):
     def get(self):
 	       self.render("home.html")
 
-class DecksListHandler(tornado.web.RequestHandler):
+class BaseHandler(tornado.web.RequestHandler):
+    def get(self):
+        return self.application.db
+    
+class DecksListHandler(BaseHandler):
     def get(self):
         entries = self.db.query("SELECT * FROM deck")
         self.render("deckslist.html", entries=entries)
@@ -49,13 +58,16 @@ class Application(tornado.web.Application):
 		(r"/viewdeck", ViewDeckHandler),
 		(r"/newcard", NewCardHandler),
 		(r"/viewcard", ViewCardHandler),
-        (r"/deckslist", DecksListHandler)
-	]
+        (r"/deckslist", DecksListHandler)]
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
 		debug = True
         )
-	tornado.web.Application.__init__(self, handlers, **settings)
+        tornado.web.Application.__init__(self, handlers, **settings)
+    
+        self.db = tornado.database.Connection(
+            host=options.mysql_host, database=options.mysql_database,
+            user=options.mysql_user, password=options.mysql_password)
 
 def main():
     tornado.options.parse_command_line()
