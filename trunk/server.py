47311122f22cff0446310b2195cf8a230287e9b0
argishti.rostamian@gmail.com <argishti.rostamian@gmail.com@70de9bcd-8a5d-4bb2-0ea2-7be2089eee11>
2011-02-06 00:20:37 +0000

    adding routes

--- trunk/server.py
+++ trunk/server.py
@@ -10,18 +10,44 @@ from tornado.options import define, options
 
 define("port", default=8888, help="run on the given port", type=int)
 
-class MainHandler(tornado.web.RequestHandler):
+class HomeHandler(tornado.web.RequestHandler):
     def get(self):
-        #self.write("Hello, world")
 	self.render("home.html")
+
+class DeckHandler(tornado.web.RequestHandler):
+    def get(self):
+	self.render("deck.html")
+
+class NewDeckHandler(tornado.web.RequestHandler):
+    def get(self):
+	self.render("newdeck.html")
+
+class ViewDeckHandler(tornado.web.RequestHandler):
+    def get(self):
+	self.render("viewdeck.html")
+
+class NewCardHandler(tornado.web.RequestHandler):
+    def get(self):
+	self.render("newcard.html")
+
+class ViewCardHandler(tornado.web.RequestHandler):
+    def get(self):
+	self.render("viewcard.html")
+
 
 class Application(tornado.web.Application):
     def __init__(self):
         handlers = [
-		(r"/", MainHandler),
+		(r"/", HomeHandler),
+		(r"/deck", DeckHandler),
+		(r"/newdeck", NewDeckHandler),
+		(r"/viewdeck", ViewDeckHandler),
+		(r"/newcard", NewCardHandler),
+		(r"/viewcard", ViewCardHandler),
 	]
         settings = dict(
             template_path=os.path.join(os.path.dirname(__file__), "templates"),
+		debug = True
         )
 	tornado.web.Application.__init__(self, handlers, **settings)
 
